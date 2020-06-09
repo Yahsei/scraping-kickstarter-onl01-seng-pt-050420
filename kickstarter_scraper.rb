a05aa5b9c9ee79a94daa655232c33fa76c965e26
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-06-09 19:35:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,5 +4,7 @@ require "pry"
 # require libraries/modules here
 
 def create_project_hash
-  # write your code here
+   html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
